cabd16f06a15a2842068aa4d1082c92ed11d6921
rajanbajaj <rajanbajajkota@gmail.com>
2024-04-17 18:18:24 +0530

    Add pages for books and reviews

---
+++ app/book/reviews/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookReviews = () => {
+  return (
+    <div>BookReviews</div>
+  )
+}
+
+export default BookReviews
--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const Menu = () => {
   return (
     <ul className="menu bg-base-200 w-56 rounded-box">
+        <li><Link href='/about'>About</Link></li>
         <li>
             <details open>
             <summary>Art Galary</summary>
@@ -15,25 +16,33 @@ const Menu = () => {
         </li>
         <li>
             <details open>
-                <summary>Travel Diaries</summary>
+            <summary>Books</summary>
+            <ul>
+                <li><Link href='/book'>List</Link></li>
+                <li><Link href='/book/reviews'>Reviews</Link></li>
+            </ul>
+            </details>
+        </li>
+        <li><Link href='/contact'>Contact</Link></li>
+        <li>
+            <details open>
+                <summary>Developer Journey</summary>
                 <ul>
-                    <li><Link href='/travel/galary'>Picture Galary</Link></li>
-                    <li><Link href='/travel/blog'>Blog</Link></li>        
+                    <li><Link href='/dev/blog'>Blog</Link></li>        
+                    <li><Link href='/dev/stories'>Stories</Link></li>
                 </ul>
             </details>
         </li>
         <li>
             <details open>
-                <summary>Developer Journey</summary>
+                <summary>Travel Diaries</summary>
                 <ul>
-                    <li><Link href='/dev/stories'>Stories</Link></li>
-                    <li><Link href='/dev/blog'>Blog</Link></li>        
+                    <li><Link href='/travel/blog'>Blog</Link></li>        
+                    <li><Link href='/travel/galary'>Picture Galary</Link></li>
                 </ul>
             </details>
         </li>
-        <li><Link href='/contact'>Contact</Link></li>
-        <li><Link href='/about'>About</Link></li>
-        </ul>
+    </ul>
   )
 }
 
