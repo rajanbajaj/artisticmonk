f756ec197a3d9016a28f77377289f87a69f2a239
rajanbajaj <rajanbajajkota@gmail.com>
2024-04-24 15:28:42 +0530

    Fix Page Layout file

--- app/layouts/Layout.tsx
+++ app/layouts/Layout.tsx
@@ -1,37 +1,27 @@
-"use client"
-import React, { useContext } from 'react';
-import MyThemeContext from '../store/MyThemeContext';
-import NavBar from '../components/NavBar/NavBar';
-import PageLayout from '../components/Page/PageLayout';
-import { Inter } from 'next/font/google';
+// layouts/Layout.tsx
+
+import React from 'react';
 import Head from 'next/head';
 
 interface LayoutProps {
+  title: string;
   children: React.ReactNode;
 }
 
-const inter = Inter({ subsets: ["latin"] });
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const themeCtx: { isDarkMode?: boolean; toggleThemeHandler: () => void } =
-    useContext(MyThemeContext);
+const Layout: React.FC<LayoutProps> = ({ title, children }) => {
   return (
-    <html data-theme={themeCtx} lang="en">
+    <div>
       <Head>
-        <title>Artistic Monk</title>
+        <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <body className={inter.className}>
-        <NavBar />
-        <div className="grid grid-rows-2 grid-flow-col gap-4">
-          <div className="col-span-8">
-            <PageLayout>
-              {children}
-            </PageLayout>
-          </div>
+      <header className="bg-gray-800 py-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-white">{title}</h1>
         </div>
-      </body>
-    </html>
+      </header>
+      <main>{children}</main>
+    </div>
   );
 };
 
