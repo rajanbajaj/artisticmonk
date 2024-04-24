21b99fee67f570b4e4d9b458251d480a84e21bb4
rajanbajaj <rajanbajajkota@gmail.com>
2024-04-24 15:22:08 +0530

    Revert "add view port meta and fix next js warning for using react context"
    This reverts commit 61fa03a6a24440b7248acc3243702ae0670993f4.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar/NavBar";
+import PageLayout from "./components/Page/PageLayout";
 import MyThemeContext, { MyThemeContextProvider } from "./store/MyThemeContext";
-import Layout from "./layouts/Layout";
+import { useContext } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Artistic Monk",
@@ -13,11 +18,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const themeCtx: { isDarkMode?: boolean; toggleThemeHandler: () => void } =
+    useContext(MyThemeContext);
   return (
     <MyThemeContextProvider>
-      <Layout>
-        {children}
-      </Layout>
+      <html data-theme={themeCtx} lang="en">
+        <body className={inter.className}>
+          <NavBar />
+          <div className="grid grid-rows-2 grid-flow-col gap-4">
+            <div className="col-span-8">
+              <PageLayout>
+                {children}
+              </PageLayout>
+            </div>
+          </div>
+        </body>
+      </html>
     </MyThemeContextProvider>
   );
 }
