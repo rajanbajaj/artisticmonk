303f0b13bfaeee22d5f75e82c4f473be5b13eb72
rajanbajaj <rajanbajajkota@gmail.com>
2024-04-25 22:20:35 +0530

    add three js library to render 3d cube at home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
+"use client"
+import { NextPage } from "next";
 import React from "react";
+import * as THREE from "three";
 
-export default function Home() {
+const Home: NextPage = () => {
+  React.useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.getElementsByClassName("image")[0].appendChild(renderer.domElement);
+
+    const texture = new THREE.TextureLoader().load("/assets/lighthouse.jpg");
+    const geometry = new THREE.BoxGeometry(4, 4, 4);
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const plane = new THREE.Mesh(geometry, material);
+    scene.add(plane);
+
+    camera.position.z = 5;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      plane.rotation.x += 0.01;
+      plane.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  }, []);
+
   return (
-    <div className="card">
-      here
-    </div>
+    <>
+      <div className="image"></div>
+    </>
   );
-}
+};
+
+export default Home;
