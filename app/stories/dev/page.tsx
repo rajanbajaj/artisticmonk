b7ef63e9cd8340d917f0815729b7d57ce56775e5
rajanbajaj <rajanbajajkota@gmail.com>
2024-05-27 22:01:23 +0530

    Add dynamic data loading for dev blogs through gist

--- app/stories/dev/page.tsx
+++ app/stories/dev/page.tsx
@@ -1,16 +1,27 @@
-import React from "react"
+"use client"
+import React, { useEffect, useState } from "react"
 import PageLayout from "../../layouts/PageLayout"
 import { Post } from "../../models/types/Post";
-import { postModel } from "../../models/Post";
 import Card from "../../components/Card/Card";
+import DOMPurify from "isomorphic-dompurify";
 
 const Developer = () => {
-  const posts: Post[] = postModel.getPosts();
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch("https://gist.githubusercontent.com/rajanbajaj/6b604317cf35127a835e8a157d28588d/raw/772d5b7efd8903057ce153f8c91e53a063d485ae/devBlogs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        data.body = DOMPurify.sanitize(data.body);
+        setPosts(data)
+      }).catch(err => console.log(err));
+  }, []);
+
   return (
     <PageLayout title="Dev Blog">
       <div className='gap-4 md:flex'>
         { posts.map((post) => (
-          <Card title={post.title} caption={post.summary} img={post.coverImage} action={{link: `/dev/blog?postId=${post.id}`, text: "Read Now"}} key={post.id}/>
+          <Card title={post.title} caption={post.summary} img={post.coverImage} action={{link: `/stories/dev/blog?postId=${post.id}`, text: "Read Now"}} key={post.id}/>
         ) 
         )}
       </div>
