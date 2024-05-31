6fc7fae693c007e72175f4de8bae84fef1a49524
rajanbajaj <rajanbajajkota@gmail.com>
2024-05-31 22:08:00 +0530

    Update gist url for dev blogs to remove gist commit version * removing git commit version returns latest gist

--- app/stories/dev/page.tsx
+++ app/stories/dev/page.tsx
@@ -9,7 +9,7 @@ const Developer = () => {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    fetch("https://gist.githubusercontent.com/rajanbajaj/6b604317cf35127a835e8a157d28588d/raw/772d5b7efd8903057ce153f8c91e53a063d485ae/devBlogs.json")
+    fetch("https://gist.githubusercontent.com/rajanbajaj/6b604317cf35127a835e8a157d28588d/raw/devBlogs.json")
       .then((res) => res.json())
       .then((data) => {
         data.body = DOMPurify.sanitize(data.body);
@@ -19,7 +19,7 @@ const Developer = () => {
 
   return (
     <PageLayout title="Dev Blog">
-      <div className='gap-4 md:flex'>
+      <div className='gap-4'>
         { posts.map((post) => (
           <Card title={post.title} caption={post.summary} img={post.coverImage} action={{link: `/stories/dev/blog?postId=${post.id}`, text: "Read Now"}} key={post.id}/>
         ) 
