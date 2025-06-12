cc07ce8124f4b7a813b891c3d849b5987dca3feb
rajanbajaj <rajanbajajkota@gmail.com>
2025-06-12 10:58:35 +0530

    Add meta tag google ad sense account

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { MyThemeContextProvider } from "./store/MyThemeContext";
 import LayoutContainer from "./layouts/LayoutContainer";
 import React from "react";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Artistic Monk",
@@ -15,10 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <MyThemeContextProvider>
-      <LayoutContainer>
-        {children}
-      </LayoutContainer>
-    </MyThemeContextProvider>
+    <>
+        <Head>
+            <meta name="google-adsense-account" content="ca-pub-2126294825019391" />
+        </Head>
+        <MyThemeContextProvider>
+        <LayoutContainer>
+            {children}
+        </LayoutContainer>
+        </MyThemeContextProvider>
+    </>
   );
 }
